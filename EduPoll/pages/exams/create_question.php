e3968b805f47fd74c6de13ac4920feee336f2d86
F0lha <pedrovieiracastro@hotmail.com>
2016-04-25 21:34:59 +0100

    add question/category/answer php

---
+++ EduPoll/pages/exams/create_question.php
@@ -0,0 +1,15 @@
+<?
+require_once ('../../config/init.php');
+include_once ('../common/utils.php');
+
+if (! isLoggedIn ()) {
+	header ( 'Location: ' . $BASE_URL . 'pages/users/login.php' );
+	die ();
+} else if (isAdmin()) {
+  	header('Location: ' . $BASE_URL . 'pages/admin/main.php');
+  	die();
+ }
+
+$smarty->assign ( 'name', $_SESSION ['name'] );
+$smarty->display ( 'exams/create.tpl' );
+?>
