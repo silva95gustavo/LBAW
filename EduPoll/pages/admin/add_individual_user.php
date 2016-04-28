b513254e85a6e6a5459b399a2d9b89384e490a9f
guilhermevpinto <guilhermevpinto@gmail.com>
2016-04-28 01:07:11 +0100

    register single user

--- EduPoll/pages/admin/add_individual_user.php
+++ EduPoll/pages/admin/add_individual_user.php
@@ -8,5 +8,9 @@ if (! isAdmin ()) {
 }
 
 $smarty->assign ( 'name', $_SESSION ['name'] );
+$smarty->assign ( 'genderValues', array(0 => 'Male', 1 => 'Female') );
+$smarty->assign ( 'genderType', 0 );
+$smarty->assign ( 'userValues', array(2 => 'Student', 1 => 'Teacher') );
+$smarty->assign ( 'userType', 2 );
 $smarty->display ( 'admin/add_individual_user.tpl' );
 ?>
