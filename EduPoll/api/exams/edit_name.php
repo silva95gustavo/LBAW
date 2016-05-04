d401773ce0594c8fb3cf1b153efef1055a7b3b00
Gustavo Silva <silva95gustavo@gmail.com>
2016-05-04 18:32:46 +0100

    ...

--- EduPoll/api/exams/edit_name.php
+++ EduPoll/api/exams/edit_name.php
@@ -3,7 +3,7 @@ require_once ('../../config/init.php');
 include_once ('../../pages/common/utils.php');
 include_once ('../../database/exams.php');
 
-$data['name'] = $_POST;
+$data['name'] = $_POST['name'];
 header('Content-Type: application/json');
 echo json_encode($data);
 
