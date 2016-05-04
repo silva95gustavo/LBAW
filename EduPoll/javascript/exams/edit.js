5686af9901b8677c9aebf83222314fbdc35abc76
aslmbc13 <asl_mbc_13@hotmail.com>
2016-05-04 17:45:21 +0100

    Edit exam name started

--- EduPoll/javascript/exams/edit.js
+++ EduPoll/javascript/exams/edit.js
@@ -2,13 +2,14 @@ function updateField(field, name, inputElement, inputSelector) {
 	var data = {
 			'id' : field.closest('.exam').attr('id').substr("exam".length, 99999)
 	}
-	data[field.attr('name')] = inputElement.val();
+	data[inputElement.attr('name')] = inputElement.val();
 	$.ajax({
-		url : "../../pages/api/exams/edit_name.php",
+		url : "../../api/exams/edit_name.php",
 		type: "POST",
 		data : data,
 		success: function(data, textStatus, jqXHR)
 		{
+			console.log(jqXHR.responseText);
 			var obj = JSON.parse(jqXHR.responseText);
 			editFieldFinish(field, nl2br(htmlspecialchars(obj[name])), inputElement, inputSelector);
 		},
