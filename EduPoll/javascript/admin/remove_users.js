cdea255f81ce088c7c66213e1d2c584bc164cf87
Gustavo Silva <silva95gustavo@gmail.com>
2016-05-11 23:22:46 +0100

    ...

--- EduPoll/javascript/admin/remove_users.js
+++ EduPoll/javascript/admin/remove_users.js
@@ -1,39 +1,41 @@
-
 var userId = null;
 
-$('#confirmationModal').on('show.bs.modal', function (e) {
-    var data = $(e.relatedTarget).data();
-    console.log(data);
-    $('#yes').data('id', data.id);
-})
+$(document).ready(
+		function() {
+			$('#confirmationModal').on('show.bs.modal', function(e) {
+				var data = $(e.relatedTarget).data();
+				console.log(data);
+				$('#yes').data('id', data.id);
+			})
 
-$('#yes').click(function (e) {
-    userId = $(this).data('id');
-    console.log('check', $(this).data('id'), userId, $(this).data());
-    $.ajax({
-        type: 'POST',
-        url: "../../actions/users/delete.php",
-        data: { id: userId },
-        success: function () {
-            $('tr#' + userId).remove();
-            $('#confirmationModal').modal('hide');
-        },
-        error: function () {
-            location.reload();
-        }
-    });
-})
-
-function searchForUserMatch() {
-    var data = $('#inputUserToRemove').val();
-    console.log(data);
-
-    $.ajax({
-        type: 'POST',
-        url: "../../actions/users/searchFTS.php",
-        data: { data: data },
-        success: function (data) {
-            console.log(data);
-        }
-    });
-}
+			$('#yes').click(
+					function(e) {
+						userId = $(this).data('id');
+						console.log('check', $(this).data('id'), userId,
+								$(this).data());
+						$.ajax({
+							type : 'POST',
+							url : "../../actions/users/delete.php",
+							data : {
+								id : userId
+							},
+							success : function() {
+								$('tr#' + userId).remove();
+								$('#confirmationModal').modal('hide');
+							},
+							error : function() {
+								location.reload();
+							}
+						});
+					})
+					$("#inputUserToRemove").autocomplete({
+						source : BASE_URL + "api/admin/search_user_by_name.php",
+						minLength : 3,
+						select : function(event, ui) {
+							console.log(ui.item ? "Selected: " + ui.item.value
+									+ " aka " + ui.item.id
+									: "Nothing selected, input was "
+										+ this.value);
+						}
+					});
+		});
