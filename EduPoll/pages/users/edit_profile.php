4174fb6a2c7e616435706da76a97b22ebfbcb76f
aslmbc13 <asl_mbc_13@hotmail.com>
2016-05-12 13:17:50 +0100

    Fixed calendar

--- EduPoll/pages/users/edit_profile.php
+++ EduPoll/pages/users/edit_profile.php
@@ -1,11 +1,14 @@
 <?
 require_once ('../../config/init.php');
 include_once ('../common/utils.php');
+include_once('../common/sidebar.php');
 
 if (! isLoggedIn ()) {
 	header ( 'Location: ' . $BASE_URL . 'pages/auth/login.php' );
 	die ();
 }
+
+prepareDate($smarty);
 
 $smarty->assign ( 'name', $_SESSION ['name'] );
 $smarty->display ( 'users/edit_profile.tpl' );
