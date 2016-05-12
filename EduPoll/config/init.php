3d4ccee0dfbde1cf4e5a30c6aad26dd911b5e7dd
Gustavo Silva <silva95gustavo@gmail.com>
2016-05-12 13:31:55 +0100

    ...

--- EduPoll/config/init.php
+++ EduPoll/config/init.php
@@ -1,20 +1,20 @@
 <?php
-  session_set_cookie_params(3600, '/~lbaw1565/proto'); // FIXME
-  //session_set_cookie_params(3600, '/~up201305803/EduPoll'); // FIXME
+  //session_set_cookie_params(3600, '/~lbaw1565/proto'); // FIXME
+  session_set_cookie_params(3600, '/~up201304143/LBAW'); // FIXME
   session_start();
 
   error_reporting(E_ERROR | E_WARNING); // E_NOTICE by default
 
-  $BASE_DIR = '/opt/lbaw/lbaw1565/public_html/proto/';
-  //$BASE_DIR = '/usr/users2/mieic2013/up201305803/public_html/EduPoll/';
-  $BASE_URL = '/~lbaw1565/proto/';
-  //$BASE_URL = '/~up201305803/EduPoll/';
+  //$BASE_DIR = '/opt/lbaw/lbaw1565/public_html/proto/';
+  $BASE_DIR = '/usr/users2/mieic2013/up201304143/public_html/LBAW/';
+  //$BASE_URL = '/~lbaw1565/proto/';
+  $BASE_URL = '/~up201304143/LBAW/';
 
   $conn = new PDO('pgsql:host=dbm;dbname=lbaw1565', 'lbaw1565', 'BN80V7U5');
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $conn->exec('SET SCHEMA \'public\'');
+  $conn->exec('SET SCHEMA \'Gustavo\'');
 
   include_once($BASE_DIR . 'lib/smarty/Smarty.class.php');
   include_once($BASE_DIR . 'lib/phpPasswordHashingLib/passwordLib.php');
