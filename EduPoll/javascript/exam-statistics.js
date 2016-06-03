73d4a591681c8a9560a9673a70e2ae7048e6a75e
aslmbc13 <asl_mbc_13@hotmail.com>
2016-06-03 03:54:34 +0100

    Maybe done, maybe not, might have bugs

--- EduPoll/javascript/exam-statistics.js
+++ EduPoll/javascript/exam-statistics.js
@@ -3,8 +3,19 @@ var circular = null;
 var bar = null;
 var chart = null;
 
+var submitted_no;
+var approved_no;
+var grade_dist = [];
 
 $(document).ready(function(){
+	submitted_no = parseInt($("#exam_submitted_no").text());
+	approved_no = parseInt($("#exam_approved_no").text());
+	
+	for(var i = 0; i <= 20; ++i) {
+		grade_dist[i] = parseInt($("#grade_distribution #grade" + i).text());
+	}
+	console.log(grade_dist);
+	
 	circular = $("#AScanvas").get(0).getContext("2d");
 	bar = $("#GDcanvas").get(0).getContext("2d");
 	asChart();
@@ -28,13 +39,13 @@ function asChart(){
 
 	data = [
 	        {
-	        	value: 43,
+	        	value: approved_no,
 	        	color: "#85e085",
 	        	highlight: "#00e64d",
 	        	label: "Approved"
 	        },
 	        {
-	        	value: 7,
+	        	value: submitted_no - approved_no,
 	        	color: "#ff4d4d",
 	        	highlight: "#ff1a1a",
 	        	label: "Disapproved"
@@ -56,9 +67,7 @@ function gdChart(){
 	            strokeColor: "#0080ff",
 	            highlightFill: "#66b3ff",
 	            highlightStroke: "#0059b3",
-	            data: [1,2,1,2,1,1,0,1,
-	                   1,1,2,5,5,1,4,5,
-	                   7,4,2,3,1]
+	            data: grade_dist
 			}
 		]
 	};
